17164b54134eb1ec563a8a13364d2020a6e0e859
Matheus LCSantos <matheuslcsantosbh@gmail.com>
2022-04-05 19:21:40 -0300

    Pegaai

---
+++ Calc/script.js
@@ -0,0 +1,14 @@
+var resultado = document.getElementById('resu')
+function clicou()
+{
+let Num1 = document.getElementsByClassName('boton')
+resultado.innerHTML=`Seu numero foi ${Num1}`
+}
+
+
+
+
+//function clicou(number)
+//{
+//    document.getElementById('resu').innerHTML += number
+//}
